cdb23222abb1e059f995f087433c338a37cf344c
BHN16 <34187868+BHN16@users.noreply.github.com>
2020-04-20 19:37:50 -0500

    Contenido dinamico: Anhadidas funciones multiplo y palindrome

--- web/server.py
+++ web/server.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 def static_content(content):
     return render_template(content)
 
-@app.route('/esprimo/<numero>')#parametro por defecto es string
+@app.route('/esprimo/<numero>')
 def es_primo(numero):
     numero = int(numero)
     if numero > 1:
@@ -26,6 +26,21 @@ def es_primo(numero):
     else:
         return "No es un numero primo"
 
+@app.route('/multiplo/<numero1>/<numero2>')
+def multiplo(numero1, numero2):
+    num1 = int(numero1)
+    num2 = int(numero2)
+    if(num1%num2 == 0):
+        return "Si es múltiplo"
+    else:
+        return "No es múltiplo"
+
+@app.route('/palindrome/<palabra>')
+def es_palidromo(palabra):
+    for i in range(0,int(len(palabra)/2)):
+        if palabra[i] != palabra[len(palabra)-1-i]:
+            return "No es palindrome"
+    return "Es palindrome"
 
 if __name__ == '__main__':
     app.secret_key = ".."
